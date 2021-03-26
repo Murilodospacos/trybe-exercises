797db6fc647b5f9b5a883dd96402dc9c078db975
murilodospacos <murilopacos@gmail.com>
2021-03-26 20:50:44 -0300

    Exercicio 1 bloco 4 dia 3 quadro de asteristicos

--- Modulo_1/bloco_4/dia_3/exercises/introducao-a-javascript-e-logica-de-programacao_3/scripts_b4d3ex2.js
+++ Modulo_1/bloco_4/dia_3/exercises/introducao-a-javascript-e-logica-de-programacao_3/scripts_b4d3ex2.js
@@ -1,3 +1,26 @@
+/* 
+Exercicio Bloco 4 dia 3 
 
+1- Para o primeiro exercício de hoje, faça um programa que, dado um valor n qualquer, seja n > 1 , imprima na tela um quadrado feito de asteriscos de lado de tamanho n . Por exemplo:
 
+n = 5
 
+*****
+*****
+*****
+*****
+*****
+
+*/
+
+let n = 5;
+let ast = '*';
+let prints = '';
+
+for (let index = 0; index < n; index +=1){
+  prints = prints + ast;
+
+}
+for (let i = 0; i < n; i += 1) {
+    console.log(prints);
+}
