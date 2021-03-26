62231bd68910d584e673456d719a156e7072e1be
murilodospacos <murilopacos@gmail.com>
2021-03-25 22:07:41 -0300

    Ex 6 - Descobrindo numero impar dentro do array e imprimindo na tela a qtda

--- Modulo_1/bloco_4/dia_2/exercises/introducao-a-javascript-e-logica-de-programacao_2/script_exercise6.js
+++ Modulo_1/bloco_4/dia_2/exercises/introducao-a-javascript-e-logica-de-programacao_2/script_exercise6.js
@@ -1,1 +1,14 @@
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let numbers = [20, 10, 6, 12, 70, 8, 100, 2, 30, 26];
+
+let numberOdd = 0;
+let contOdd = 0;
+
+for (let index = 0; index < numbers.length; index +=1) {
+     if( numbers[index] % 2 > 0 ){
+      numberOdd += 1;
+     } 
+}
+if(numberOdd === 0) {  
+  console.log('nenhum valor ímpar encontrado')
+}
+console.log('O Array possui '+ numberOdd + ' númreos ímpares !')
