c12bf16b8742f941d2c94feffd8bd87ba12c0a12
murilodospacos <murilopacos@gmail.com>
2021-03-26 07:40:38 -0300

    ex 9 - concluido - o resultado do ex 8 dividir cada elemento por 2 e imprimir

--- Modulo_1/bloco_4/dia_2/exercises/introducao-a-javascript-e-logica-de-programacao_2/script_exercise8.js
+++ Modulo_1/bloco_4/dia_2/exercises/introducao-a-javascript-e-logica-de-programacao_2/script_exercise8.js
@@ -1,6 +1,6 @@
 let numbers = [];
 
-for (let index = 0; index < 26; index += 1) {
+for (let index = 1; index < 26; index += 1) {
   numbers.push(index);
 }
 console.log(numbers);
--- Modulo_1/bloco_4/dia_2/exercises/introducao-a-javascript-e-logica-de-programacao_2/script_exercise9.js
+++ Modulo_1/bloco_4/dia_2/exercises/introducao-a-javascript-e-logica-de-programacao_2/script_exercise9.js
@@ -1,1 +1,6 @@
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let numbers = [];
+
+for (let index = 1; index < 26; index += 1) {
+  numbers.push(index / 2);
+}
+console.log(numbers);
