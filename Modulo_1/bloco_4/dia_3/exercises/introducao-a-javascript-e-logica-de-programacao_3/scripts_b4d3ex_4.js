cb763f8035ea75b03a9735d55cfcb2413011b8ca
murilodospacos <murilopacos@gmail.com>
2021-03-30 11:25:42 -0300

    Exercicio 4 bloco 4 dia 3

---
+++ Modulo_1/bloco_4/dia_3/exercises/introducao-a-javascript-e-logica-de-programacao_3/scripts_b4d3ex_4.js
@@ -0,0 +1,31 @@
+/* 
+Exercicio Bloco 4 dia 3 
+
+4- Depois, faça uma pirâmide com n asteriscos de base:
+n = 5
+
+  *
+ ***
+*****
+
+*/
+const pyranmid = (n) => {
+  const midPoint = Math.floor((n * 2 - 1) / 2) 
+  
+  for (let row = 0; row < n; row += 1) {
+  let level = "";
+  
+  for (let col = 0; col < n * 2 - 1 /*9*/; col += 1) {
+  if (midPoint - row <= col && midPoint + row >= col) {
+  level += "*";
+  } else {
+  level += " ";
+  }
+  }
+  console.log(level);
+  }
+  }
+  
+  const numberOfAst = 50;
+  
+  pyranmid(numberOfAst); 
