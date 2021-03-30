94ebf3a8e953b2535f1ef615e3855553dfd1d2e0
murilodospacos <murilopacos@gmail.com>
2021-03-30 10:46:53 -0300

    Alteracao consolelog por return

--- Modulo_1/bloco_4/dia_4/Exercicios/exercises/introducao-a-javascript-e-logica-de-programacao_4/scripts_funcoes_parte1.js
+++ Modulo_1/bloco_4/dia_4/Exercicios/exercises/introducao-a-javascript-e-logica-de-programacao_4/scripts_funcoes_parte1.js
@@ -2,16 +2,20 @@
 function yesOrNopalindromo (word) {
 
   for(let i = 0; i < word.length; i += 1) {
-    console.log(word[i] + word[word.length-1-i])
+    // for percorre todos caracteres
+
+    //console.log(word[i] + word[word.length-1-i])
+    // Primeira impressão imprime palavra da esquerda para direita e outro imprimi da direita para esquerda
     if(word[i] === word[word.length-1-i]) {
+      // Se o primeiro caractere for igual aao ultimo caractere e durante o loop os otros caracteres forem iguais
       result = true;
+      // resultado igual a true
     } else {
+      // se os caracteres não forem iguais resultado igual false
       result = false;
     }
   }
-  console.log(result)
+  return result;// imprime resultado da lógica do if
 
 }
-
-yesOrNopalindromo('ovo');
-
+console.log(yesOrNopalindromo('subinoonibus'));
