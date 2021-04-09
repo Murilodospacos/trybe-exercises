098f512c21748a0bc01708031140f62be59d4f61
murilodospacos <murilopacos@gmail.com>
2021-04-08 21:03:30 -0300

    Exercicio bloco 5 dia 1 parte 3

--- Modulo_1/bloco_5/dia_1/exercises/5_1/scripts_b5d1p3.js
+++ Modulo_1/bloco_5/dia_1/exercises/5_1/scripts_b5d1p3.js
@@ -3,3 +3,7 @@ document.getElementById("header-container").style.backgroundColor = "green";
 document.getElementsByClassName("emergency-tasks")[0].style.backgroundColor = "red";
 
 document.getElementsByClassName("no-emergency-tasks")[0].style.backgroundColor = "yellow";
+
+document.querySelectorAll(".emergency-talks-h3")[0]
+
+document.querySelector(".myFooter").style.backgroundColor = "green";
