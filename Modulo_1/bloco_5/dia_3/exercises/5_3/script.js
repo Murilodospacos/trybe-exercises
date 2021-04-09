bec521a4f969884f68ba3bbe32d4ff6193e89dcd
murilodospacos <murilopacos@gmail.com>
2021-04-09 10:39:37 -0300

    Exercicio 1 concluido calendary

--- Modulo_1/bloco_5/dia_3/exercises/5_3/script.js
+++ Modulo_1/bloco_5/dia_3/exercises/5_3/script.js
@@ -14,8 +14,37 @@ function createDaysOfTheWeek() {
 createDaysOfTheWeek();
 
 // Escreva seu código abaixo.
- function createListCalendary (){
-   let linesList = document.createElement('li');
-   let days = document.querySelector('#days');
 
- }
+const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+
+ function createListDaysCalendary() {
+   let mydays = document.querySelector('#days');
+   for (let index = 0; index < dezDaysList.length; index += 1) {
+    let day = dezDaysList[index];
+    let dayListItem = document.createElement('li');
+    if (day === 24 || day === 31){
+      dayListItem.className = 'day holiday';
+      dayListItem.innerHTML = day;
+      mydays.appendChild(dayListItem);
+    } else if (day === 4 || day === 11 || day === 18) {
+      dayListItem.className = 'day friday';
+      dayListItem.innerHTML = day;
+      mydays.appendChild(dayListItem);
+    } else if (day === 25) {
+      dayListItem.className = 'day holiday friday';
+      dayListItem.innerHTML = day;
+      mydays.appendChild(dayListItem)
+    } else {
+      dayListItem.className = 'day';
+      dayListItem.innerHTML = day;
+      mydays.appendChild(dayListItem);
+    }
+  };
+}
+
+createListDaysCalendary();
+
+function holidays (Holidaysmonth) {
+  
+}
+
