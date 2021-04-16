6155400898ea28afdad6411786e4c002a42a7546
murilodospacos <murilopacos@gmail.com>
2021-04-16 15:49:19 -0300

    Exercicio bloco 6 dia 1 pos aula ao vivo

--- Modulo_1/bloco_6/dia_1/exercises/html-e-css-forms-flexbox-e-responsivo_1/script.js
+++ Modulo_1/bloco_6/dia_1/exercises/html-e-css-forms-flexbox-e-responsivo_1/script.js
@@ -1,15 +1,31 @@
-const myCountry = document.getElementById('country');
+const myCountry = document.getElementById('country'); // Capturando elemento html.
 
+// Criando o campo de Estado dinamicamente.
 function countryBrazil() {
   const estadosBrasil = [
     'Acre','Alagoas','Amapá','Amazonas','Bahia','Ceará','Distrito Federal','Espírito Santo','Goías','Maranhão','Mato Grosso','Mato Grosso do Sul','Minas Gerais','Pará','Paraíba','Paraná','Pernambuco','Piauí','Rio de Janeiro','Rio Grande do Norte','Rio Grande do Sul','Rondônia','Roraíma','Santa Catarina','São Paulo','Sergipe','Tocantins'
   ]; 
 
-  for (let index = 0; index < estadosBrasil.length; index += 1) {
-    const country = document.createElement('option');
-    myCountry.appendChild(country).innerText = estadosBrasil[index];
-    myCountry.appendChild(country).value = estadosBrasil[index];
+  for (let index = 0; index < estadosBrasil.length; index += 1) {      // Pecorrendo o Array
+    const country = document.createElement('option');                  // criando elemento.
+    myCountry.appendChild(country).innerText = estadosBrasil[index];   // Adicionando elemento com o texto recebido do array
+    myCountry.appendChild(country).value = estadosBrasil[index];       // Adicionado Elemento com o valor recebido do array
   }
 }
 countryBrazil();
+//--------------------------------------------------------------------------------------------------------------------------
 
+
+const mydate = document.getElementById('date').value;
+
+function validateDate(mydate) {
+  if (mydate.indexOf('/') === 2 || mydate.indexOf('/') === 5) {
+    const day = mydate.substr(0, 2);
+    const month = data.substr(3, 2);
+    const year = data.substr(6, 4);
+    if((day > 0 && day <=31) && (month > 0 && month <=12) && (year >= 0 && year.length === 4)) {
+      return true;
+    }
+  }
+  return false;
+}
