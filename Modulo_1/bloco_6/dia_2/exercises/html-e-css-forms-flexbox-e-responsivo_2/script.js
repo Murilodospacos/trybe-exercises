1c13d52e54404ffafcc1ea6dd576ff474e098531
murilodospacos <murilopacos@gmail.com>
2021-04-18 18:57:25 -0300

    instalando bibliotecas moment, just-validate e piday

--- Modulo_1/bloco_6/dia_2/exercises/html-e-css-forms-flexbox-e-responsivo_2/script.js
+++ Modulo_1/bloco_6/dia_2/exercises/html-e-css-forms-flexbox-e-responsivo_2/script.js
@@ -2,7 +2,7 @@
 
   function createStateOptions() {
     const states = document.getElementById('country');
-    const stateOptions = ['Selecione seu estado', 'AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MG', 'MS', 'MT', 'PA', 'PB', 'PE', 'PI', 'PR', 'RJ', 'RN', 'RO', 'RR', 'RS', 'SC', 'SE', 'SP', 'TO'];
+    const stateOptions = ['Selecione seu estado', 'Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Distrito Federal', 'Espirito Santo', 'Goiás', 'Maranhão', 'Mato Grosso do Sul', 'Mato Grosso', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins'];
     for (let index = 0; index < stateOptions.length; index += 1) {
       const createOptions = document.createElement('option');
       states.appendChild(createOptions).innerText = stateOptions[index];
