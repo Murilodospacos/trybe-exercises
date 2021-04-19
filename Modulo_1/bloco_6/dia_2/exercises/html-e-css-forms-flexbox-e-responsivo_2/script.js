049796af0344b197dd16f27f34afb22177601543
murilodospacos <murilopacos@gmail.com>
2021-04-18 21:22:22 -0300

    Tentando fazer a validacao dos campos dos formularios

--- Modulo_1/bloco_6/dia_2/exercises/html-e-css-forms-flexbox-e-responsivo_2/script.js
+++ Modulo_1/bloco_6/dia_2/exercises/html-e-css-forms-flexbox-e-responsivo_2/script.js
@@ -10,135 +10,38 @@
     }
   }
   createStateOptions();
+  // Adicionando biblioteca para data, somente datas validas
+  document.getElementById('myInputId').DatePickerX.init();
 
-  const picker = new Pikaday({
-    field: document.getElementById('datepicker'),
-    format: 'D/M/YYYY',
-    toString(date, format) {
-      // you should do formatting based on the passed format,
-      // but we will just return 'D/M/YYYY' for simplicity
-      const day = date.getDate();
-      const month = date.getMonth() + 1;
-      const year = date.getFullYear();
-      return `${day}/${month}/${year}`;
-    },
-    parse(dateString, format) {
-      // dateString is the result of `toString` method
-      const parts = dateString.split('/');
-      const day = parseInt(parts[0], 10);
-      const month = parseInt(parts[1], 10) - 1;
-      const year = parseInt(parts[2], 10);
-      return new Date(year, month, day);
-    }
+  // Validando todos os campos dos formulários
+  validation.init("#myForm");
+
+  validation.init("#myForm", {
+    events: ["change", "paste", "keyup"]
   });
-  
-  const clearButton = document.querySelector('.clear-button');
-  function clearFields() {
-    const formElements = document.querySelectorAll('input');
-    const textArea = document.querySelector('textarea')
-    for (let index = 0; index < formElements.length; index += 1) {
-      const userInput = formElements[index];
-      userInput.value = '';
-      textArea.value = '';
-    }
+
+  const Rules = validation.getRules();
+  Rules["custom"] = {
+    message: "Error Message here",
+    method: el => {
+      // ... rule here ...
+    }    
   }
-  
-  clearButton.addEventListener('click', clearFields);
-  
-  new JustValidate('.js-form', {
-    rules: {
-      name: {
-        required: true,
-        minLength: 3,
-        maxLength: 40
-      },
-      email: {
-        required: true,
-        email: true,
-        maxLength: 50
-      },
-      cpf: {
-        required: true,
-        maxLength: 11
-      },
-      address: {
-        required: true,
-        maxLength: 200
-      },
-      city: {
-        required: true,
-        maxLength: 28
-      },
-      state: {
-        required: true,
-      },
-      radio: {
-        required: true,
-      },
-      text: {
-        required: true,
-        maxLength: 1000
-      },
-      position: {
-        required: true,
-        maxLength: 40
-      },
-      description: {
-        required: true,
-        maxLength: 500
-      },
-      date: {
-        required: true,
-      }
-    },
-    messages: {
-      name: {
-        required: 'O campo de nome é obrigatório.',
-        maxLength: 'O limite é de 40 caracteres.'
-      },
-      email: {
-        required: 'O campo de email é obrigatório.',
-        email: 'O email digitado não é válido.',
-        maxLength: 'O limite é de 50 caracteres.'
-      },
-      cpf: {
-        required: 'O campo de CPF é obrigatório.',
-        maxLength: 'O limite é de 11 caracteres.'
-      },
-      address: {
-        required: 'O campo endereço é obrigatório.',
-        maxLength: 'O limite é de 200 caracteres.'
-      },
-      city: {
-        required: 'O campo cidade é obrigatório.',
-        maxLength: 'O limite é de 28 caracteres.'
-      },
-      state: {
-        required: 'O campo estado é obrigatório.',
-      },
-      radio: {
-        required: 'A escolha de um item é obrigatória.',
-      },
-      text: {
-        required: 'Este campo é obrigatório.',
-        maxLength: 'O limite é de 1000 caracteres.'
-      },
-      position: {
-        required: 'Este campo é obrigatório.',
-        maxLength: 'O limite é de 40 caracteres.'
-      },
-      description: {
-        required: 'Este campo é obrigatório.',
-        maxLength: 'O limite é de 500 caracteres.'
-      },
-      date: {
-        required: 'Este campo é obrigatório.',
-      }
-    },
-    submitHandler: function (form, values) {
-      console.log(form, values);
-    }});
-  
-  window.onload = function () {
-    createStateOptions();
-  }
+
+  // Show errors
+validation.highlight();
+
+// Hide all errors
+validation.hideAll();
+
+// Check if all the input fields inside of a container are valid (body by default)
+validation.isValid(); // returns true or false
+
+// isValid() + highlight()
+validation.validate(); // returns true or false
+
+// Show a custom popup message on any DOM element
+validation.show(el, message);
+
+// Hide the popup message from the DOM element
+validation.hide(el);
