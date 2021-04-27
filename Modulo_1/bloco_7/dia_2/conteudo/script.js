f1bae767d9a5fb9ec852008224e66b43a6d12aa8
murilodospacos <murilopacos@gmail.com>
2021-04-27 10:10:14 -0300

    Praticando exercicios e exemplos do bloco 7 dia 2

---
+++ Modulo_1/bloco_7/dia_2/conteudo/script.js
@@ -0,0 +1,123 @@
+// Exemplo 1 Parte I - Adicionando novas chamves (keys)
+const customer1 = {
+  firstName: 'Roberto',
+  age: 22,
+  job: 'Teacher',
+};
+
+console.log(customer1);
+
+customer1.lastName = 'Faria';
+console.log(customer1);
+
+const customer2 = {
+  firstName: 'Maria',
+  age: 23,
+  job: 'Medic',
+};
+
+console.log(customer2);
+customer2['lastName'] = 'Silva';
+console.log(customer2);
+
+// Exemplo 2  Parte I - Adicionando novas chamves (keys)
+
+const customer = {
+  firstName: 'Roberto',
+  age: 22,
+  job: 'Teacher',
+};
+
+let newKey = 'lastName';
+const lastName = 'Ferreira';
+customer[newKey] = lastName;
+newKey = 'fullName';
+const fullName = `${customer.firstName} ${customer.lastName}`;
+customer[newKey] = fullName;
+console.log(customer);
+
+// Praticando
+
+function returnValues (objeto, nome, valor) {
+  let objeto = {};
+  objeto[nome] = valor;
+
+  return objeto;
+}
+
+returnValues(sexo, seu , murilo);
+
+// Exemplo 1 Parte II - Object.keys
+
+const student1 = {
+  Html: 'Muito Bom',
+  Css: 'Bom',
+  JavaScript: 'Ótimo',
+  SoftSkills: 'Ótimo',
+};
+
+const student2 = {
+  Html: 'Bom',
+  Css: 'Ótimo',
+  JavaScript: 'Ruim',
+  SoftSkills: 'Ótimo',
+  Git: 'Bom', // chave adicionada
+};
+
+// Tente criar uma função que exiba as habilidades do objeto student . Cada habilidade deve ser exibida no formato "Nome da habilidade, Nível: valor da chave referente à habilidade". Após tentar criar a função, veja a sugestão de resposta logo abaixo e compare com a sua solução.
+
+const listSkills = (student) => {
+  const arraySkills = Object.keys(student);
+  for(index in arraySkills) {
+    console.log(`${arraySkills[index]}, Nível: ${student[arraySkills[index]]}`)
+  }
+};
+
+console.log('Estudante 1');
+listSkills(student1);
+
+console.log('Estudante 2');
+listSkills(student2);
+
+// Exemplo 1 Parte III - Object.values
+
+const student = {
+  Html: 'Muito Bom',
+  Css: 'Bom',
+  JavaScript: 'Ótimo',
+  SoftSkill: 'Ótimo',
+};
+
+const listSkillsWithFor = (student) => {
+  const skills = [];
+  for(skill in student) {
+    skills.push(student[skill]);
+  }
+
+  return skills;
+};
+
+const listSkillsWithValues = (student) => Object.values(student);
+
+// Sem Object.values
+console.log(listSkillsWithFor(student));
+
+// Com Object.values
+console.log(listSkillsWithValues(student));
+
+// Exemplo 1 - Parte IV - Object.entries
+
+const países = {
+  França: 'Paris',
+  Brasil: 'Brasília',
+  Espanha: 'Madrid',
+  Portugal: 'Lisboa',
+};
+const pairKeyValue = Object.entries(países);
+console.log(pairKeyValue);
+
+for(index in pairKeyValue) {
+  console.log('--------');
+  console.log('Pais:', pairKeyValue[index][0]);
+  console.log('Capital:', pairKeyValue[index][1]);
+};
