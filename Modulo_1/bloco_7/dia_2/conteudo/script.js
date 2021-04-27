f43faf260c329c21f0445371d6e959f31355261b
murilodospacos <murilopacos@gmail.com>
2021-04-27 10:11:22 -0300

    Praticando exercicios e exemplos do bloco 7 dia 2

--- Modulo_1/bloco_7/dia_2/conteudo/script.js
+++ Modulo_1/bloco_7/dia_2/conteudo/script.js
@@ -121,3 +121,41 @@ for(index in pairKeyValue) {
   console.log('Pais:', pairKeyValue[index][0]);
   console.log('Capital:', pairKeyValue[index][1]);
 };
+
+// Exemplo I - Parte V - Object.assign
+
+// A função recebe um número qualquer de parâmetros. Todos são agregados como valores para adicionar ao objeto de destino!
+
+Object.assign(destino, objeto1);
+Object.assign(destino, objeto1, objeto2);
+Object.assign(destino, objeto1, objeto2, objeto3, objeto4);
+
+// Exemplo II - Parte V - Object.assign 
+
+const person = {
+  name: 'Alberto',
+  lastName: 'Gomes',
+  age: 20,
+};
+
+const info = {
+  age: 23,
+  job: 'engenheiro',
+};
+
+const family = {
+  children: ['Maria', 'João'],
+  wife: 'Ana',
+};
+
+Object.assign(person, info, family)
+console.log(person)
+
+/* Output
+  { name: 'Alberto',
+  lastName: 'Gomes',
+  age: 23,
+  job: 'engenheiro',
+  child: [ 'Maria', 'João' ],
+  wife: 'Ana'
+  } */
