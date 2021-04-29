b1936e0b3344d4b1b843ff5811e306ee89a275ec
murilodospacos <murilopacos@gmail.com>
2021-04-29 13:00:25 -0300

    Exercicio 1 parte II

--- Modulo_1/bloco_7/dia_3/exercises/introducao-a-javascript-es6-e-testes-unitarios_3/scriptp2Ex1.js
+++ Modulo_1/bloco_7/dia_3/exercises/introducao-a-javascript-es6-e-testes-unitarios_3/scriptp2Ex1.js
@@ -1,22 +1,14 @@
 const assert = require('assert');
+// escreva a função addOne aqui
+const addOne = (array) => {
+  
+}
 
-const obj1 = {
-  title: 'My Title',
-  description: 'My Description',
-};
+const myArray = [31, 57, 12, 5];
+const unchanged = [31, 57, 12, 5];
+const expected = [32, 58, 13, 6];
+const output = addOne(myArray);
 
-const obj2 = {
-  description: 'My Description',
-  title: 'My Title',
-};
-
-const obj3 = {
-  title: 'My Different Title',
-  description: 'My Description',
-};
-
-// implemente seus testes aqui
-
-//assert.deepStrictEqual(obj1, obj2);
-//assert.deepStrictEqual(obj1, obj3);
-assert.deepStrictEqual(obj2, obj3);
+assert.strictEqual(typeof addOne, 'function');
+assert.deepStrictEqual(output, expected);
+assert.deepStrictEqual(myArray, unchanged);
