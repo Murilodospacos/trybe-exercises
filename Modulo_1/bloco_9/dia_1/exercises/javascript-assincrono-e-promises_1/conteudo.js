ce07f7318b9c6d9a3e0bc2d9aab6ce0583320930
murilodospacos <murilopacos@gmail.com>
2021-05-11 20:25:21 -0300

    exercicios do dia bloco 9 dia 1

---
+++ Modulo_1/bloco_9/dia_1/exercises/javascript-assincrono-e-promises_1/conteudo.js
@@ -0,0 +1,31 @@
+// setTimeout(() => {
+//   console.log('Comprar parafusos') // Comprar parafusos
+//   console.log('Adicionar ao estoque') // Adicionar ao estoque
+// }, 2000);
+
+// console.log('1 - Receber roda'); // 1 - Receber roda
+// console.log('2 - Encaixar parafusos'); // 2 - Encaixar parafusos
+// console.log('3 - Fixar roda no carro'); // 3 - Fixar roda no carro
+
+// const assert = require('assert');
+
+// const pushNumber = (list, number) => list.push(number);
+
+// let numbers = [];
+
+// pushNumber(numbers, 1);
+// pushNumber(numbers, 2);
+// pushNumber(numbers, 3);
+
+// assert.deepStrictEqual(numbers, [1, 2, 3]);
+
+const monthlyBudget = (myIncome, myExpenses, callback) => {
+
+  const totalExpenses = callback(myExpenses);
+  const totalAfterExpenses = myIncome - totalExpenses;
+
+  console.log(`Balanço do mês:
+    Recebido: R$${myIncome},00
+    Gasto: R$${totalExpenses},00
+    Saldo: R$${totalAfterExpenses},00 `);
+};
