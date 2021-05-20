37ab9614573a353b04589877dd9a8d493e790b9e
murilodospacos <murilopacos@gmail.com>
2021-05-20 09:07:27 -0300

    corrigindo erros

--- Modulo_1/bloco_10/dia_2/exercises/testes-automatizados-com-jest_2/Testes/exercise1.spec.js
+++ Modulo_1/bloco_10/dia_2/exercises/testes-automatizados-com-jest_2/Testes/exercise1.spec.js
@@ -1,4 +1,4 @@
-const uppercase = require('./exercise1');
+const uppercase = require('../Exercicios/exercise1');
 
 it(`Test checks function call uppercase 'trybe' to equal 'TRYBE'`, done => {
   uppercase('trybe', (str) => {
