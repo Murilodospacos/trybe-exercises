72eb0ad7d2fbd4f9a67577cefdd394265c02e809
murilodospacos <murilopacos@gmail.com>
2021-06-11 11:06:19 -0300

    finalizado exercicios de fixaçao bloco 13.2 dos requisito 1 ao 13

--- Modulo_2/bloco_13/dia_2/exercises/ciclo-de-vida-de-componentes-e-react-router_2/comprehension-exercises-router-master/src/App.js
+++ Modulo_2/bloco_13/dia_2/exercises/ciclo-de-vida-de-componentes-e-react-router_2/comprehension-exercises-router-master/src/App.js
@@ -1,18 +1,46 @@
 import React, { Component } from 'react';
 import Home from './components/Home';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import About from './components/About';
+import Users from './components/Users';
+import StrictAccess from './components/StrictAccess'
+
+// Requisito 9
+const Child = ({ match })  => (
+  <div>
+    <h3>ID: {match.params.users.id}</h3>
+  </div>
+)
 
 class App extends Component {
   render() {
     return (
       // requisito 1
-      <BrowserRouter>
-        <Home />
-        {/* Requisito 2 */}
-        <Route exact path="/" component={ Home } />
-        {/* Requisito 3 */}
-        <Route path="/about" component={ About } />
-      </BrowserRouter>
+      <Router>
+         <ul>
+          {/* Requisito 6 */}
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/users/3">Users</Link></li>
+          <li><Link to="/strict-access">Strict Access</Link></li>
+        </ul>
+        <Switch>
+          {/* Requisito 2 */}
+          <Route exact path="/" component={ Home } />
+          {/* Requisito 3 e 4 */}
+          <Route path="/about" component={ About } />
+          {/* Requisito 5 e 8 */}
+          <Route 
+           path="/users/:id" 
+           render={ (props) => 
+            <Users {...props} greetingMessage="Good Morning" /> }/> 
+          {/* Requisito 9 */}
+          <Route path="users/:id" component={Child} />
+          <Route path="/strict-access">
+           <StrictAccess user={{ username: 'joa', password: '1234'}} />
+           </Route>
+        </Switch>
+      </Router>
     );
   }
 }
