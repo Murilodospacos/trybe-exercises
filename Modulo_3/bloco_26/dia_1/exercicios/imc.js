728e741e3b8288b6f9a31eb0f7d2aee5e1c63e91
murilodospacos <murilopacos@gmail.com>
2021-09-24 17:45:14 -0300

    exercicio 2 e 3

--- Modulo_3/bloco_26/dia_1/exercicios/imc.js
+++ Modulo_3/bloco_26/dia_1/exercicios/imc.js
@@ -1,10 +1,13 @@
 const readline = require('readline-sync');
-const peso_65KG = 65;
-const altura_1M_77CM = 177;
+// const peso_65KG = 65; -- exercicio 1
+// const altura_1M_77CM = 177; -- exercicio 1
 
 function calculaIMC() {
-  const peso = peso_65KG;
-  const altura = altura_1M_77CM;
+  // const peso = peso_65KG; -- exercicio 1
+  // const altura = altura_1M_77CM; -- exercicio 1
+  const peso = readline.questionInt('Qual o seu peso? (em kg)');
+  const altura = readline.questionInt('Qual a sua altura? (em cm)');
+
 
   console.log(`Peso: ${peso}, Altura: ${altura}`);
   const imc = (peso / Math.pow(altura / 100, 2)).toFixed(2);
