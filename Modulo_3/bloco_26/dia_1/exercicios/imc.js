a38871560af613ebf0c2281033caa20a91e077e6
murilodospacos <murilopacos@gmail.com>
2021-09-24 18:00:20 -0300

    exercicio 5

--- Modulo_3/bloco_26/dia_1/exercicios/imc.js
+++ Modulo_3/bloco_26/dia_1/exercicios/imc.js
@@ -16,6 +16,21 @@ function calculaIMC() {
   // sitaxe : Math.pow(base, expoente)
 
   console.log(`IMC: ${imc}`);
+
+  console.log('Situação do IMC:');
+  if(imc < 18.5){
+    console.log('Abaixo do peso (magreza)')
+  } else if(imc >= 18.5 && imc < 24.9) {
+    console.log('Peso normal');
+  } else if(imc >= 25 && imc < 30) {
+    console.log('Acima do peso (sobrepeso)');
+  } else if(imc >= 30 && imc < 35) {
+    console.log('Obesidade grau I');
+  } else if(imc > 35 && imc < 40) {
+    console.log('Obesidade grau II');
+  } else {
+    console.log('Obesidade graus III e IV');
+  }
 }
 
 calculaIMC();
