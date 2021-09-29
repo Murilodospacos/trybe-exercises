845305a522ca8d5a78b9925c0d6186680473d42f
murilodospacos <murilopacos@gmail.com>
2021-09-29 15:28:19 -0300

    iniciando exercicio 1

---
+++ Modulo_3/bloco_26/dia_3/exercicios/tests/readFileTest.js
@@ -0,0 +1,15 @@
+describe('Chama a função', () => {
+  describe('Quando a função é executada com sucesso', () => {
+    describe('a resposta', () => {
+
+      it('é uma string', async() => {})
+      it('é igual ao conteudo do arquivo', async() => {})
+    })
+  })
+
+  describe('Quando ocorre algum erro na execução da função', () => {
+    describe('a resposta', () => {
+      it('é igual a "null"', async () => {})
+    })
+  })
+})
