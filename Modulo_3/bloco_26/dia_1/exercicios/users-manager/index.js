9ab83634df038a19883ede223dfe204c62ea4f44
murilodospacos <murilopacos@gmail.com>
2021-10-15 17:32:09 -0300

    exercicios do dia 26.1

---
+++ Modulo_3/bloco_26/dia_1/exercicios/users-manager/index.js
@@ -0,0 +1,19 @@
+// Importamos as libs que vamos usar
+const express = require('express');
+const bodyParser = require('body-parser');
+const middlewares = require('./middlewares');
+
+// Criamos a aplicação do express
+const app = express();
+
+// Instalamos o middleware que fez a leitura e conversão do corpo das requisições em JSON
+app.use(bodyParser.json());
+
+app.post('/user', middlewares.createUser);
+app.use(middlewares.error);
+
+// definimos a porta
+const PORT = 3000;
+
+// iniciamos o servidor
+app.listen(PORT, () => { console.log(`App listening on port ${PORT}`) })
